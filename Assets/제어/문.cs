40f25d5154439d71c3af1325692d994451c7442c
steen11 <96470689+steen11@users.noreply.github.com>
2022-01-29 19:36:44 +0900

    드로도콜수정

--- Assets/제어/문.cs
+++ Assets/제어/문.cs
@@ -9,6 +9,10 @@ public class 문 : UdonSharpBehaviour
     public Animator Gate_Doors_1;
     public override void Interact()
     {
+        SendCustomNetworkEvent(VRC.Udon.Common.Interfaces.NetworkEventTarget.All, "문싱크");
+    }
+    public void 문싱크()
+    {
         Gate_Doors_1.SetTrigger("istri");
     }
 }
