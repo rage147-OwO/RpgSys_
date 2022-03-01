207c1d19ef6040d3cdb9f80bedc7ec6d8f3eb4cd
steen11 <96470689+steen11@users.noreply.github.com>
2022-03-01 12:47:17 +0900

    포션추가사안
    당근시스템은 고장이 난 프로젝트

---
+++ Assets/월드애니메이션/리셋장치.cs
@@ -0,0 +1,14 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class 리셋장치 : UdonSharpBehaviour
+{
+    public Animator Reset;
+    public override void Interact()
+    {
+        Reset.Play("공속포션리셋", -1, 0f);
+    }
+}
