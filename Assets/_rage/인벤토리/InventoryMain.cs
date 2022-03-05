756bd3d759b63b2d10dcbc72d094984ef52082d7
steen11 <96470689+steen11@users.noreply.github.com>
2022-03-05 13:40:04 +0900

    포션추가
    당근강화버전에서 포션을 추가하고
    스폰지역을 수정한 버전

--- Assets/_rage/인벤토리/InventoryMain.cs
+++ Assets/_rage/인벤토리/InventoryMain.cs
@@ -10,8 +10,8 @@ public class InventoryMain : UdonSharpBehaviour
     public inventoryDesktop Deskop;
     public VRbag VR;
     public GameObject VR더미;
-    public GameObject 이동버튼;
-    public GameObject 버튼;
+    public GameObject 브이알;
+    public GameObject 피씨;
 
 
     private void Start()
@@ -19,15 +19,15 @@ public class InventoryMain : UdonSharpBehaviour
         if (Networking.LocalPlayer.IsUserInVR()) {
             Deskop.gameObject.SetActive(false);
             VR더미.gameObject.SetActive(true);
-            이동버튼.gameObject.SetActive(true);
-            버튼.gameObject.SetActive(false);
+            브이알.gameObject.SetActive(true);
+            피씨.gameObject.SetActive(false);
         }
         else
         {
             Deskop.gameObject.SetActive(true);
             VR더미.gameObject.SetActive(false);
-            이동버튼.gameObject.SetActive(false);
-            버튼.gameObject.SetActive(true);
+            브이알.gameObject.SetActive(false);
+            피씨.gameObject.SetActive(true);
         }
     }
 }
