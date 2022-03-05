db362e3c445ee4b924884e8e2bd9723de1646efd
rage147-OwO <bbbaa1004@gmail.com>
2022-03-05 18:19:09 +0900

    버튼추가

---
+++ Assets/_rage/던전시스템/텔레포트.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class 텔레포트 : UdonSharpBehaviour
+{
+    public Transform 텔레포트위치;
+    public GameObject 디자인;
+    public override void Interact()
+    {
+        디자인.gameObject.SetActive(true);
+        Networking.LocalPlayer.TeleportTo(텔레포트위치.position, 텔레포트위치.rotation);
+    }
+}
